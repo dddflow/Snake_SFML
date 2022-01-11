9dbaf9cf3ee2a54449a769755020dad7df5b3d00
Egor <bargov.ev@edu.spbstu.ru>
2022-01-11 18:12:39 +0300

    Change assembly pve_game

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -1689,7 +1689,7 @@ void game_pve()
                 {
                     xor ah, ah
                     mov first1, ah
-                    mov first2, ah
+                    mov eaten1, ah
                 }
             }
             if (eaten2)
